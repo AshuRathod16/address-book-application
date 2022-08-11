5dfbfaf7283f538c4e0b5f1f4e5b04bff629ae5a
Qa-krushna <rashwini510@gmail.com>
2022-08-11 21:14:25 +0530

    Added: Create a rest controller

--- src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
@@ -1,34 +1,11 @@
 package com.bridgelabz.addressbookapp.controller;
 
-import com.bridgelabz.addressbookapp.model.Contact;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/addressbookservice")
+@RequestMapping("/addressbook")
 public class AddressBookController {
-    @RequestMapping(value = {"", "/", "/get"})
-    public String welcomeUser() {
-        return "Welcome to address book home";
-    }
-
-    @GetMapping("/get/{id}")
-    public String welcomeSpecificUser(@PathVariable String id) {
-        return "Welcome, User " + id;
-    }
-
-    @PostMapping("/post")
-    public String createContact(@RequestBody Contact contact) {
-        return "Added " + contact.getName() + " to list";
-    }
-
-    @PutMapping("/update")
-    public String updateContact(@RequestBody Contact contact) {
-        return "Updated " + contact.getName() + " in list";
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public String deleteContact(@PathVariable String id) {
-        return "Deleted contact " + id;
-    }
 
 }
