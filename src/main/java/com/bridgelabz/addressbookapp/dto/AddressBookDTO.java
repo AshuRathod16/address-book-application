90cb4acac84f63cb6012236ea196f13ef1ab9f98
Qa-krushna <rashwini510@gmail.com>
2022-08-11 23:52:57 +0530

    Added: Adding model and dto class to address book app

---
+++ src/main/java/com/bridgelabz/addressbookapp/dto/AddressBookDTO.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.addressbookapp.dto;
+
+import lombok.Data;
+
+@Data
+public class AddressBookDTO {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private long phoneNumber;
+    private int zip;
+
+
+}
---
+++ src/main/java/com/bridgelabz/addressbookapp/model/AddressBookModel.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.addressbookapp.model;
+
+import com.bridgelabz.addressbookapp.dto.AddressBookDTO;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "AddressBook")
+public class AddressBookModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private long phoneNumber;
+    private int zip;
+    private LocalDateTime registerDate;
+    private LocalDateTime updateDate;
+
+    public AddressBookModel(AddressBookDTO addressBookDTO) {
+        this.firstName = addressBookDTO.getFirstName();
+        this.lastName = addressBookDTO.getLastName();
+        this.address = addressBookDTO.getAddress();
+        this.city = addressBookDTO.getCity();
+        this.state = addressBookDTO.getState();
+        this.phoneNumber = addressBookDTO.getPhoneNumber();
+        this.zip = addressBookDTO.getZip();
+    }
+
+    public AddressBookModel() {
+    }
+}
