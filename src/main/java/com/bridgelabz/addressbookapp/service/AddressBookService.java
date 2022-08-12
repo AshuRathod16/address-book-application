2d9e326166753850a99ac8b41568de077471730b
Qa-krushna <rashwini510@gmail.com>
2022-08-13 00:33:24 +0530

    Added: Save the data in address book repository from database

--- src/main/java/com/bridgelabz/addressbookapp/service/AddressBookService.java
+++ src/main/java/com/bridgelabz/addressbookapp/service/AddressBookService.java
@@ -1,7 +1,12 @@
 package com.bridgelabz.addressbookapp.service;
 
+import com.bridgelabz.addressbookapp.repository.AddressBookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddressBookService implements IAddressBookService{
+
+    @Autowired
+    AddressBookRepository addressBookRepository;
 }
